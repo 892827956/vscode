f93030510a81a96b1cb4953d78d5c8f19c9b70b6
892827956 <892827956@qq.com>
2022-01-23 11:23:35 +0800

    add index,cpp

---
+++ index.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-23 11:18:06
+ * @LastEditTime: 2022-01-23 11:18:07
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vscode\index.cpp\
+ */
