7ae15b28e221f9fe5e8b3539c1b4819c1c0b0946
892827956 <892827956@qq.com>
2022-01-23 11:32:17 +0800

    第二次提交

--- index2.cpp
+++ index2.cpp
@@ -1,10 +1,8 @@
 /*
  * @Author: your name
- * @Date: 2022-01-23 11:18:06
- * @LastEditTime: 2022-01-23 11:31:08
+ * @Date: 2022-01-23 11:31:38
+ * @LastEditTime: 2022-01-23 11:31:38
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \vscode\index.cpp\
+ * @FilePath: \vscode\index2.cpp
  */
-
-其实这是主色页湿湿的
